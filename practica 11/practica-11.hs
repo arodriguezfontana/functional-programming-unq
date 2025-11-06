2ee07e1533a5e03c963edba8095af78c8c2bdbd9
arodriguezfontana <arodriguezfontana@gmail.com>
2025-11-06 00:20:13 -0300

    wip p-11

--- practica 11/practica-11.hs
+++ practica 11/practica-11.hs
@@ -37,9 +37,7 @@ conElDobleDeAceitunas :: Pizza -> Pizza
 conElDobleDeAceitunas p = conCapasTransformadas duplicarSiAceituna p
 
 -- 3
-pizzaProcesada :: (Ingrediente -> b -> b) -> b -> Pizza -> b
-pizzaProcesada f z Prepizza = z
-pizzaProcesada f z (Capa i p) = f i (pizzaProcesada f z p)
+
 
 -- 4
 
@@ -48,10 +46,77 @@ pizzaProcesada f z (Capa i p) = f i (pizzaProcesada f z p)
 -- 6
 
 -- 7
+map :: (a -> b) -> [a] -> [b]
+map _ [] = []
+map f (x:xs) = f x : (map f xs)
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = []
+filter p (x:xs) = if p x
+    then x : filter p xs
+    else filter p xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ z [] = z
+foldr f z (x:xs) = f x (foldr f z xs)
+
+recr :: b -> (a -> [a] -> b -> b) -> [a] -> b
+recr z _ [] = z
+recr z f (x:xs) = f x xs (recr z f xs)
+
+foldr1 :: (a -> a -> a) -> [a] -> a
+foldr1 f [] = error "La lista no puede ser vacía."
+foldr1 f [x] = x
+foldr1 f (x:xs) = f x (foldr1 f xs)
+
+zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith f [] _ = []
+zipWith f _ [] = []
+zipWith f (x:xs) (y:ys) = f x y : zipWith f xs ys
+
+--
+scanr :: (a -> b -> b) -> b -> [a] -> [b]
+scanr f z [] = [z]
+scanr f z (x:xs) = f x (head (scanr f z xs)) : (scanr f z xs)
 
 -- 8
 
 -- 9
+sum :: [Int] -> Int
+sum = foldr (\n m -> n + m) 0
+
+length :: a -> Int
+length = foldr (\_ n -> 1 + n) 0
+
+map :: (a -> b) -> [a] -> [b]
+map g = foldr (\x ys -> g x : ys) []
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter p = foldr (\x ys -> if p x then x : ys else ys) []
+
+find :: (a -> Bool) -> [a] -> Maybe a
+find p = foldr (\x my -> if p x then Just x else my) Nothing
+
+any :: (a -> Bool) -> [a] -> Bool
+find p = foldr (\x b -> p x || b) False
+
+all :: (a -> Bool) -> [a] -> Bool
+all p = foldr (\x b -> p x && b) True
+
+countBy :: (a -> Bool) -> [a] -> Int
+countBy p = foldr (\x n -> if p x then 1 + n else n) 0
+
+partition :: (a -> Bool) -> [a] -> ([a], [a])
+partition p = foldr (\x (ys,zs) -> if p x then (x:ys,zs) else (ys,x:zs)) ([],[])
+
+zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+
+
+scanr :: (a -> b -> b) -> b -> [a] -> [b]
+takeWhile :: (a -> Bool) -> [a] -> [a]
+take :: Int -> [a] -> [a]
+drop :: Int -> [a] -> [a]
+elemAt :: Int -> [a] -> a
 
 -- 10
 
